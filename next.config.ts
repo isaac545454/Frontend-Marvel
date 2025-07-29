9f253866fe9a404fd3394e63af2c56838802a287
isaac545454 <isaac.newton1221@gmail.com>
2025-07-29 15:41:02 -0300

    chore: configuring local image and call host

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['http://172.26.240.1'],
+  images: {
+    domains: ['i.annihil.us'],  
+  },
   /* config options here */
 };
 
