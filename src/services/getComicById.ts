e51af3eaaee3d063abe4873eab43f71bd0dfb395
isaac gomes <isaac.newton1221@gmail.com>
2025-10-30 14:34:20 -0300

    Update getComicById.ts

--- src/services/getComicById.ts
+++ src/services/getComicById.ts
@@ -2,14 +2,23 @@ import { Comic } from '@/types/Comic'
 import { MarvelResponse } from '@/types/MarvelResponse'
 import { md5 } from '@/lib/md5'
 
-export async function getComicById(id: string): Promise<Comic> {
+export async function getComicById(id: string): Promise<Comic | null> {
   const { ts, hash, apikey } = md5()
 
   const response = await fetch(
     `https://gateway.marvel.com/v1/public/comics/${id}?ts=${ts}&apikey=${apikey}&hash=${hash}`,
-    { next: { revalidate: 60 * 60 * 24 } }  
+    {
+      next: { revalidate: 60 * 60 * 24 },
+      cache: 'force-cache',
+    }
   )
 
+  if (!response.ok) {
+    throw new Error('Erro ao buscar quadrinho')
+  }
+
   const data: MarvelResponse<Comic> = await response.json()
-  return data.data.results[0]
+  const comic = data.data.results?.[0] ?? null
+
+  return comic
 }
